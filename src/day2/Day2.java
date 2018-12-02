a11b150148ca52d44975e1a4f55d6aa6e498dc27
richardmcmullin <richard.e.mcmullin@gmail.com>
2018-12-02 16:07:59 -0500

    Added day2 framework

---
+++ src/day2/Day2.java
@@ -0,0 +1,45 @@
+package day2;
+
+import solver.AdventOfCodeSolver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day2 extends AdventOfCodeSolver {
+
+    public static void main(String[] args) {
+        new Day2().solve();
+    }
+
+    /* **********************************************************************************
+     * Read the input
+     * **********************************************************************************/
+    protected void readInput() {
+
+        while (true) {
+            String inputLine = scanner.nextLine();
+
+            if (inputLine.trim().isEmpty()) {
+                break;
+            }
+
+        }
+    }
+
+    /* **********************************************************************************
+     * Part 1
+     * **********************************************************************************/
+    protected void solvePart1() {
+
+        System.out.println("Unsolved");
+    }
+
+    /* **********************************************************************************
+     * Part 2
+     * **********************************************************************************/
+    protected void solvePart2() {
+
+        System.out.println("Unsolved");
+    }
+
+}
